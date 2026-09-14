7540de00affbe0fb59f5d1ed234d24a0a3caf6e4
bunnicula <>
2026-09-14 22:09:43 +0200

    aggiunta launcher (per jar standalone)

---
+++ src/main/java/fractalvisualizer/Launcher.java
@@ -0,0 +1,14 @@
+package fractalvisualizer;
+
+import fractalvisualizer.view.MainApp;
+import javafx.application.Application;
+
+public final class Launcher {
+
+    private Launcher() {
+    }
+
+    public static void main(String[] args) {
+        Application.launch(MainApp.class, args);
+    }
+}
